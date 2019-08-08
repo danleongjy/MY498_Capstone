a39059f4ba33465ccbba67faaa16e802cd64220b
danleongjy <daniel.leongjy@gmail.com>
2019-08-08 13:40:05 +0100

    copy edits to paper, tested code for run only with provided data

--- 01 Data Prep/tfl_journeyplanner_api_settings.py
+++ 01 Data Prep/tfl_journeyplanner_api_settings.py
@@ -1,5 +1,5 @@
 def get_app_id():
-    return '' # specify app_id from TfL - actually can be blank
+    return '' # specify app_id from TfL
 
 def get_app_key():
-    return '' # specify app_key from TfL - actually can be blank
+    return '' # specify app_key from TfL
